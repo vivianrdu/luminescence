0c1a35d4a1ae75dde5d2f8b3092f29cae9868856
vivianrdu <vivian_du@berkeley.edu>
2022-02-16 21:23:49 -0800

    Connect start menu to sample scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+
+    #region Unity_functions
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    #region Scene_transitions
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void WinGame()
+    {
+        SceneManager.LoadScene("EndCutscene");
+    }
+    #endregion
+}
