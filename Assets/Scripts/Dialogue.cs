ace5a712b32dbbba4ed407ffe882b2b7c8dcb33b
vivianrdu <vivian_du@berkeley.edu>
2022-02-17 19:58:28 -0800

    attempted dialogue

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour
+[System.Serializable]
+public class Dialogue
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [TextArea(3, 10)]
+    public string[] sentences;
 }
