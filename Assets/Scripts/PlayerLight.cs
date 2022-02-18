80c961f8a81155e0918f80ad8717c1a845f0833f
vivianrdu <vivian_du@berkeley.edu>
2022-02-17 16:57:13 -0800

    add firefly slider

--- Assets/Scripts/PlayerLight.cs
+++ Assets/Scripts/PlayerLight.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
 
 public class PlayerLight : MonoBehaviour
 {
     Light2D playerLight;
     float originalRange;
 
+    #region Firefly_counter
+    float currFirefly;
+    public Slider FireflySlider;
+    #endregion
+
     private void Awake()
     {
         playerLight = GetComponent<Light2D>();
         originalRange = playerLight.pointLightOuterRadius;
+        currFirefly = 0;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Firefly"))
@@ -26,5 +34,6 @@ public class PlayerLight : MonoBehaviour
     {
         originalRange += 1f;
         playerLight.pointLightOuterRadius = originalRange;
+        FireflySlider.value += 1;
     }
 }
